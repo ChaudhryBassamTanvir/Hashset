ed789bd97b88c89654974df7723615d993d389ac
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-12-04 11:10:37 +0500

    Updated

--- MashMapPractice.java
+++ MashMapPractice.java
@@ -1,5 +1,6 @@
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class MashMapPractice {
     public static void main(String[] args) {
@@ -24,11 +25,28 @@ public class MashMapPractice {
     //Search 
     if(map.containsKey("China")){
         System.out.println("Yes Present in map " +map.get("China"));
+        System.out.println();
 
 
     }else{
         System.out.println("Key is not present");
     }
 
-    }
+System.out.println(map.get("China"));
+System.out.println(map.get("Indonesia"));
+
+
+
+System.out.println();
+// int arr[]={12,15,18};
+// for(int val:arr){
+//     System.out.println(val);
+// }
+// System.out.println();
+
+for(Map.Entry<String, Integer> e : map.entrySet()){
+    System.out.println(e.getKey());
+    System.out.println(e.getValue());
+}
+}
 }
