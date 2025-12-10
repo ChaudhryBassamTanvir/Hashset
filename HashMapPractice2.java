fc7d05e3d43595e560b0c3386430b35469b6c6e7
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-12-10 12:09:49 +0500

    Updated

--- HashMapPractice2.java
+++ HashMapPractice2.java
@@ -1,5 +1,43 @@
+
+import java.util.LinkedList;
+
 public class HashMapPractice2 {
+
+    public class HashMapCode{
+    static class HashMap(K,V)//generics{
+        private class Node{
+            K Key;
+            V value;
+        public Node(K key, V value){
+            this.key= key;
+            this.value=value;
+        }
+        }
+        private int n;//n
+        private int N;
+        private LinkedList<Node> buckets[]; // N=buckets.length
+
+        
+
+        @SuppressWarnings("unchecked")
+        public HashMap(){
+            this.N=4;
+            this.buckets=new LinkedList[4];
+            for(int i=0;i<4;i++){
+                this.buckets[i] = new LinkedList<>();
+
+            }
+        }
+    }
+public void put(K key, V value){
+
+}
+
+
+    }
     public static void main(String[] args) {
         //internally hashmap worked as an array of linkedList
+
+
     }
 }
