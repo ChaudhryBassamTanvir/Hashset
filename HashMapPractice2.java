5d4dc647bc7af3606a8fcac827a2f47bc52a3359
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2025-12-11 11:24:11 +0500

    Rehashing:

--- HashMapPractice2.java
+++ HashMapPractice2.java
@@ -29,10 +29,23 @@ public class HashMapPractice2 {
             }
         }
     private int hashFunction(K key){
-
+int bi= key.hashCode(); //return a random number
+Math.abs(bi)%N;
     }
-    private 
-public void put(K key, V value){ 
+
+    private int searchInLL(K key, int bi){
+        LinkedList<Node> ll= buckets[bi];
+        int di=0;
+        for(int i=0;i<ll.size();i++){
+            if (ll.get(i).key==key) {
+
+                
+                return i;//di
+            }
+        }
+        return -1;
+    }
+    private public void put(K key, V value){ 
 
    int bi= hashFunction(key);
     int di= searchInLL(key, bi);//data index di=value(valid)
@@ -40,13 +53,29 @@ if (di==-1) { //key doesn't exits
 buckets[bi].add(new Node(key, value));
     n++;
 }else{//key exist
-Node data=buckets[bi].get(di);
+Node node=buckets[bi].get(di);
 data.value=value;
 }
 
+
+private void rehash(){
+    LinkedList<Node> oldBucket[]=buckets;
+    buckets = new LinkedList[N*2];
+
+for(int i=0;i<N*2;i++){
+    buckets[i]= new LinkedList<>();
+}
+
+
+    for(int i=0;i<oldBucket.length;i++){
+        Node node =ll.get(j);
+        put(node.key, node.value);
+    }
+}
 double lamda= (double)n/N;
 if (lamda>2.0) {
     //rehashing
+    rehash()
     
 }
 
@@ -58,6 +87,9 @@ if (lamda>2.0) {
     public static void main(String[] args) {
         //internally hashmap worked as an array of linkedList
 
-
+HashMap<String, Integer> map = new HashMap<>();
+map.put("India", 120);
+map.put("Pakistan",30);
+map.put("China", 120);
     }
 }
